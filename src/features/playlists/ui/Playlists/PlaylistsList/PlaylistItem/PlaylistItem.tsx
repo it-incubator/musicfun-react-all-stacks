f1b8ebd5d1a08471a6624168db7a9b254f89da68
safronman <safronmanbox@gmail.com>
2025-05-21 15:04:00 +0300

    refactor: update playlist - 2

--- src/features/playlists/ui/Playlists/PlaylistsList/PlaylistItem/PlaylistItem.tsx
+++ src/features/playlists/ui/Playlists/PlaylistsList/PlaylistItem/PlaylistItem.tsx
@@ -1,26 +1,26 @@
-import type { PlaylistItem as PlaylistItemType } from "../../../../api/playlistsApi.types.ts"
+import type { Playlist } from "../../../../api/playlistsApi.types.ts"
 
 type Props = {
-  playlist: PlaylistItemType
-  editPlaylist: (playlist: PlaylistItemType | null) => void
+  playlist: Playlist
+  editPlaylist: (playlist: Playlist | null) => void
 }
 
 export const PlaylistItem = ({ playlist, editPlaylist }: Props) => {
-  const { attributes } = playlist
+  const { title, description, tags, addedAt } = playlist.attributes
 
   return (
     <div>
       <div>
-        <b>title:</b> <span>{attributes.title}</span>
+        <b>title:</b> <span>{title}</span>
       </div>
       <div>
-        <b>description:</b> <span>{attributes.description}</span>
+        <b>description:</b> <span>{description}</span>
       </div>
       <div>
-        <b>tags:</b> <span>{attributes.tags.length ? attributes.tags.map((t) => t) : "Теги не добавлены"}</span>
+        <b>tags:</b> <span>{tags.length ? tags.map((t) => t) : "Теги не добавлены"}</span>
       </div>
       <div>
-        <b>added date:</b> <span>{new Date(attributes.addedAt).toLocaleDateString()}</span>
+        <b>added date:</b> <span>{new Date(addedAt).toLocaleDateString()}</span>
       </div>
       <button onClick={() => editPlaylist(playlist)}>Редактировать</button>
     </div>
