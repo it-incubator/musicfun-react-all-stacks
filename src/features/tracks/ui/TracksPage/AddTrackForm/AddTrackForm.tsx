737b5f26cd4a66ccac6dd679ecffc2fe9b260831
safronman <safronmanbox@gmail.com>
2025-05-23 12:06:26 +0300

    feat: add track - 2

--- src/features/tracks/ui/TracksPage/AddTrackForm/AddTrackForm.tsx
+++ src/features/tracks/ui/TracksPage/AddTrackForm/AddTrackForm.tsx
@@ -1,5 +1,6 @@
+import { PlaylistQueryKey, playlistsApi } from "@/features/playlists/api/playlistsApi.ts"
 import { type ChangeEvent, useState } from "react"
-import { useMutation } from "@tanstack/react-query"
+import { useMutation, useQuery } from "@tanstack/react-query"
 import { type SubmitHandler, useForm } from "react-hook-form"
 import { toast } from "react-toastify"
 import type { Nullable } from "@/common"
@@ -8,24 +9,32 @@ import { TrackQueryKey, tracksApi } from "../../../api/tracksApi.ts"
 
 export const AddTrackForm = () => {
   const [file, setFile] = useState<Nullable<File>>(null)
+  const [playlistId, setPlaylistId] = useState<Nullable<string>>(null)
 
   const {
     register,
     handleSubmit,
+    reset,
     formState: { isValid },
   } = useForm<{ title: string }>({ mode: "onChange" })
-
-  console.log("AddTrackForm")
 
   const { mutate, isPending } = useMutation({
     mutationFn: tracksApi.createTrack,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [TrackQueryKey] })
       toast("Трек успешно загружен", { type: "success", theme: "colored" })
+      reset()
+      setFile(null)
+      setPlaylistId(null)
     },
     onError: () => {
       toast("Ошибка загрузки трека", { type: "error", theme: "colored" })
     },
+  })
+
+  const { data } = useQuery({
+    queryKey: [PlaylistQueryKey, "my"],
+    queryFn: playlistsApi.fetchMyPlaylists,
   })
 
   const uploadHandler = (e: ChangeEvent<HTMLInputElement>) => {
@@ -46,23 +55,45 @@ export const AddTrackForm = () => {
   }
 
   const onSubmit: SubmitHandler<{ title: string }> = ({ title }) => {
-    if (!file) return
-
-    // TODO: Можно ли загрузить трек не указывая плейлист?
-    const playlistId = "fc815b2c-3ebe-4f8c-aae7-e54652dfea1f"
+    if (!playlistId || !file) return
     mutate({ playlistId, title, file })
   }
+
+  const isSubmitDisabled = !playlistId || !file || !isValid || isPending
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <h2>Добавить новый трек</h2>
       <div>
-        <input {...register("title")} placeholder="Введите название трека" />
+        <label>
+          Введите название трека:
+          <input {...register("title")} placeholder="Введите название трека" />
+        </label>
       </div>
       <div>
-        <input type="file" accept="audio/*" onChange={uploadHandler} />
+        <label>
+          Загрузите аудио файл:
+          <input type="file" accept="audio/*" onChange={uploadHandler} />
+        </label>
       </div>
-      <button type="submit" disabled={isPending || !isValid || !file}>
+
+      <div>
+        <label>
+          Выберите плейлист:
+          <select onChange={(e) => setPlaylistId(e.target.value)} value={playlistId ?? ""}>
+            <option value="" disabled>
+              -- Выберите плейлист --
+            </option>
+            {data?.data.data.map((playlist) => (
+              <option key={playlist.id} value={playlist.id}>
+                {playlist.attributes.title}
+              </option>
+            ))}
+          </select>
+        </label>
+      </div>
+
+      <button type="submit" disabled={isSubmitDisabled}>
         {isPending ? "Загрузка..." : "Добавить трек"}
       </button>
     </form>
