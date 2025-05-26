647842acb88a1d5a61f5ed1acc6a8970058a055e
safronman <safronmanbox@gmail.com>
2025-05-26 09:33:09 +0300

    feat: add API-KEY

--- src/common/instance/instance.ts
+++ src/common/instance/instance.ts
@@ -2,6 +2,9 @@ import axios from "axios"
 
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    "API-KEY": import.meta.env.VITE_API_KEY,
+  },
 })
 
 instance.interceptors.request.use((config) => {
