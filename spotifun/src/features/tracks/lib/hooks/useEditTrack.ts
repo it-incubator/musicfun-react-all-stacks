7e41c456649a3c8ffef473c07e518f48f13fc7b1
safronman <safronmanbox@gmail.com>
2025-05-30 12:05:39 +0300

    refactor: useEditTrack

--- spotifun/src/features/tracks/lib/hooks/useEditTrack.ts
+++ spotifun/src/features/tracks/lib/hooks/useEditTrack.ts
@@ -6,7 +6,7 @@ import type { MouseEvent } from "react"
 import { useState } from "react"
 import { type SubmitHandler, useForm } from "react-hook-form"
 import { TrackQueryKey, tracksApi } from "../../api/tracksApi.ts"
-import type { UpdateTrackArgs } from "../../api/tracksApi.types.ts"
+import type { BaseAttributes, TrackDetails, UpdateTrackArgs } from "../../api/tracksApi.types.ts"
 
 export const useEditTrack = () => {
   const [trackId, setTrackId] = useState<Nullable<string>>(null)
@@ -22,9 +22,10 @@ export const useEditTrack = () => {
     onError: (err: unknown) => showErrorToast("Ошибка при обновлении трека", err),
   })
 
-  // const editTrack = (e: MouseEvent, track: Nullable<TrackDetails<PlaylistItemAttributes>>) => {
-  const editTrack = (e: MouseEvent, track: any) => {
-    debugger
+  // TracksList -         Nullable<TrackDetails<FetchTracksAttributes>>
+  // TrackPage -          Nullable<TrackDetails<TrackDetailAttributes>>
+  // PlaylistTracks -     Nullable<TrackDetails<PlaylistItemAttributes>>
+  const editTrack = <T extends BaseAttributes>(e: MouseEvent, track: Nullable<TrackDetails<T>>) => {
     e.preventDefault()
 
     setTrackId(track?.id ?? null)
@@ -32,7 +33,8 @@ export const useEditTrack = () => {
     if (track) {
       const { attributes } = track
       // TODO: как мне подтянуть свойства трека (например lyrics),
-      //  чтобы их можно было обновить если в треке не содержится эта информация
+      // чтобы их можно было обновить если в треке не содержится эта информация
+      // Это можно реализовать только на странице трека TrackPage
       reset({ title: attributes.title })
     }
   }
