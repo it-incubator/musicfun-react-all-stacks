579b05ac6da99a814420846a734a7450c761c87f
safronman <safronmanbox@gmail.com>
2025-05-30 11:44:12 +0300

    feat: add track to playlist on TrackPage.tsx

--- spotifun/src/features/tracks/ui/TrackPage/TrackPage.tsx
+++ spotifun/src/features/tracks/ui/TrackPage/TrackPage.tsx
@@ -1,6 +1,8 @@
 import { Layout, Loader, PageTitle } from "@/common/components"
 import { Path } from "@/common/routing"
+import { useAddToPlaylist } from "@/features/tracks/lib/hooks/useAddToPlaylist.ts"
 import { useRemoveTrack } from "@/features/tracks/lib/hooks/useRemoveTrack.ts"
+import { AddTrackToPlaylistModal } from "@/features/tracks/ui/TracksPage/AddTrackToPlaylistModal/AddTrackToPlaylistModal.tsx"
 import { useQuery } from "@tanstack/react-query"
 import { Link, Navigate, useNavigate, useParams } from "react-router"
 import { TrackQueryKey, tracksApi } from "../../api/tracksApi.ts"
@@ -20,6 +22,7 @@ export const TrackPage = () => {
   const { removingTrackId, removeTrack } = useRemoveTrack(() => {
     navigate(Path.Tracks)
   })
+  const { modalTrackId, setModalTrackId, addTrackToPlaylist, openModal } = useAddToPlaylist()
 
   if (!trackId) return <Navigate to={Path.NotFound} />
 
@@ -28,18 +31,26 @@ export const TrackPage = () => {
   if (!data) return <h1>Трек не найден</h1>
 
   return (
-    <Layout>
-      <Link className={"link"} to={Path.Tracks}>
-        Вернуться назад
-      </Link>
-      <PageTitle>Информация о треке</PageTitle>
-      <TrackItem<TrackDetailAttributes> track={data?.data.data}>
-        <div>
-          <button onClick={(e) => removeTrack(e, trackId)} disabled={removingTrackId === trackId}>
-            {removingTrackId === trackId ? "Удаление..." : "Удалить"}
-          </button>
-        </div>
-      </TrackItem>
-    </Layout>
+    <>
+      <AddTrackToPlaylistModal
+        open={!!modalTrackId}
+        onClose={() => setModalTrackId(null)}
+        onSave={addTrackToPlaylist}
+      />
+      <Layout>
+        <Link className={"link"} to={Path.Tracks}>
+          Вернуться назад
+        </Link>
+        <PageTitle>Информация о треке</PageTitle>
+        <TrackItem<TrackDetailAttributes> track={data?.data.data}>
+          <div>
+            <button onClick={(e) => openModal(e, trackId)}>Добавить трек в плейлист</button>
+            <button onClick={(e) => removeTrack(e, trackId)} disabled={removingTrackId === trackId}>
+              {removingTrackId === trackId ? "Удаление..." : "Удалить"}
+            </button>
+          </div>
+        </TrackItem>
+      </Layout>
+    </>
   )
 }
