5c0146d2748584cd6446dfce730c36025cc3fa7a
safronman <safronmanbox@gmail.com>
2025-06-05 10:46:47 +0300

    refactor: delete test dnd code

--- spotifun/src/common/routing/Routing.tsx
+++ spotifun/src/common/routing/Routing.tsx
@@ -1,7 +1,6 @@
 import { Main } from "@/app/Main.tsx"
 import { PageNotFound } from "@/common/components"
 import { ArtistsPage } from "@/features/artists/ui/ArtistsPage/ArtistsPage.tsx"
-import { DndPage } from "@/features/dnd/DndPage/DndPage.tsx"
 import { PlaylistPage } from "@/features/playlists/ui/PlaylistPage/PlaylistPage.tsx"
 import { PlaylistsPage } from "@/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx"
 import { TagsPage } from "@/features/tags/ui/TagsPage/TagsPage.tsx"
@@ -16,7 +15,6 @@ export const Path = {
   Tracks: "/tracks",
   Artists: "/artists",
   Tags: "/tags",
-  Dnd1: "/dnd1",
   NotFound: "*",
 } as const
 
@@ -29,7 +27,6 @@ export const Routing = () => (
     <Route path={`${Path.Tracks}/:trackId`} element={<TrackPage />} />
     <Route path={Path.Artists} element={<ArtistsPage />} />
     <Route path={Path.Tags} element={<TagsPage />} />
-    <Route path={Path.Dnd1} element={<DndPage />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
   </Routes>
 )
