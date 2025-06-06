83acb4c9929330e70dae21b8ef20eb1ed707d0f6
safronman <safronmanbox@gmail.com>
2025-06-06 11:05:08 +0300

    refactor: track description - 2

--- spotifun/src/features/tracks/ui/TracksPage/TracksList/EditTrackForm/EditTrackForm.tsx
+++ spotifun/src/features/tracks/ui/TracksPage/TracksList/EditTrackForm/EditTrackForm.tsx
@@ -1,9 +1,7 @@
-import { artistsKey, tagsKey } from "@/common/apiEntities"
-import { artistsApi } from "@/features/artists/api/artistsApi.ts"
-import { tagsApi } from "@/features/tags/api/tagsApi.ts"
-import { useQuery } from "@tanstack/react-query"
 import { type Dispatch, type MouseEvent, type SetStateAction } from "react"
 import { type SubmitHandler, type UseFormHandleSubmit, type UseFormRegister } from "react-hook-form"
+import { EditTrackFormArtists } from "./EditTrackFormArtists/EditTrackFormArtists.tsx"
+import { EditTrackFormTags } from "./EditTrackFormTags/EditTrackFormTags.tsx"
 import type { UpdateTrackArgs } from "../../../../api/tracksApi.types.ts"
 import s from "./EditTrackForm.module.css"
 
@@ -28,71 +26,21 @@ export const EditTrackForm = ({
   artistsIds,
   setArtistsIds,
 }: Props) => {
-  const { data: tagsResponse } = useQuery({ queryKey: [tagsKey], queryFn: () => tagsApi.findTags("") })
-  const { data: artistsResponse } = useQuery({ queryKey: [artistsKey], queryFn: () => artistsApi.findArtists("") })
-
-  const toggleTag = (tagId: string) => {
-    setTagIds((prev) => (prev.includes(tagId) ? prev.filter((id) => id !== tagId) : [...prev, tagId]))
-  }
-
-  const toggleArtist = (artistId: string) => {
-    setArtistsIds((prev) => (prev.includes(artistId) ? prev.filter((id) => id !== artistId) : [...prev, artistId]))
-  }
-
   return (
     <form onSubmit={handleSubmit(onSubmit)} className={`item item--fullwidth`}>
       <h2>Редактировать трек</h2>
 
       <div className={s.container}>
-        {/* title */}
         <div>
           <h4 className={s.subTitle}>Описание трека</h4>
           <input {...register("title")} placeholder="Title" />
         </div>
-
-        {/* lyrics */}
         <div>
           <h4 className={s.subTitle}>Лирика</h4>
           <input {...register("lyrics")} placeholder="Lyrics" />
         </div>
-
-        {/* tags */}
-        <div>
-          <h4 className={s.subTitle}>Выбери тег из списка</h4>
-          <div className={"tagsList"}>
-            {tagsResponse?.data.map((tag) => {
-              const isSelected = tagIds.includes(tag.id)
-              return (
-                <div
-                  key={tag.id}
-                  onClick={() => toggleTag(tag.id)}
-                  className={`tagItem ${isSelected ? "selectedTag" : ""}`}
-                >
-                  # {tag.name}
-                </div>
-              )
-            })}
-          </div>
-        </div>
-
-        {/* Artists */}
-        <div>
-          <h4 className={s.subTitle}>Выбери артиста из списка</h4>
-          <div className={"artistList"}>
-            {artistsResponse?.data.map((artist) => {
-              const isSelected = artistsIds.includes(artist.id)
-              return (
-                <div
-                  key={artist.id}
-                  onClick={() => toggleArtist(artist.id)}
-                  className={`artistItem ${isSelected ? "selectedArtist" : ""}`}
-                >
-                  🎙️ {artist.name}
-                </div>
-              )
-            })}
-          </div>
-        </div>
+        <EditTrackFormTags tagIds={tagIds} setTagIds={setTagIds} />
+        <EditTrackFormArtists artistsIds={artistsIds} setArtistsIds={setArtistsIds} />
       </div>
 
       <button type={"submit"}>Сохранить</button>
