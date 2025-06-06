ee9f367736a52c6bc2a54a0108cc945b52b3da1f
dimych <kuzyuberdin@gmail.com>
2025-06-06 19:30:03 +0400

    add websocket

--- spotifun/src/main.tsx
+++ spotifun/src/main.tsx
@@ -1,10 +1,11 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-import { StrictMode } from "react"
+import { useEffect } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
 import { BrowserRouter } from "react-router"
 import { App } from "./app/App.tsx"
+import { getSharedSocket } from "@/socket.ts"
 
 export const queryClient = new QueryClient({
   defaultOptions: {
@@ -22,12 +23,36 @@ export const queryClient = new QueryClient({
 })
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <WebSocketProvider>
         <App />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </BrowserRouter>
-  </StrictMode>,
+      </WebSocketProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </BrowserRouter>,
 )
+
+function WebSocketProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const socket = getSharedSocket(import.meta.env.VITE_AUTH_TOKEN)
+
+    socket.on("track-added-to-playlist", (data: TrackAddedToPlaylistPayload) => {
+      console.log(data)
+    })
+  }, [])
+
+  return <>{children}</>
+}
+
+type TrackAddedToPlaylistPayload = {
+  type: "track-added-to-playlist"
+  payload: {
+    trackId: string
+    trackTitle: string
+    playlistId: string
+    playlistTitle: string
+    userId: string
+    userName: string
+  }
+}
