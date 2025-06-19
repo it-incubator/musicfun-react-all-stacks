3dd139918a8154de47a0dae7fde2bde4fc863ddb
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-19 19:08:52 +0200

    sp-6 feat: add artists api

---
+++ apps/spotifun-rtk/src/features/artists/api/artistsApi.ts
@@ -0,0 +1,31 @@
+import { baseApi } from "@/app/api/baseApi.ts"
+import type { Artist } from "@it-incubator/spotifun-api-sdk"
+
+export const artistsApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    findArtists: build.query<Artist[], string>({
+      query: (name) => ({
+        url: `/artists/search?term=${name}`,
+        method: "GET",
+      }),
+      providesTags: ["Artist"],
+    }),
+    createArtist: build.mutation<Artist[], string>({
+      query: (name) => ({
+        url: "/artists",
+        method: "POST",
+        body: { name },
+      }),
+      invalidatesTags: ["Artist"],
+    }),
+    deleteArtist: build.mutation<void, string>({
+      query: (id) => ({
+        url: `/artists/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Artist"],
+    }),
+  }),
+})
+
+export const { useFindArtistsQuery, useCreateArtistMutation, useDeleteArtistMutation } = artistsApi
