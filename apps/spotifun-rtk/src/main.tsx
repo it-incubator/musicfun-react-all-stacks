9b441179f5ec33e108b7e52d9fbe1d6d841db119
Natallia <natalyasafarevich@gmail.com>
2025-06-20 16:12:55 +0200

    SP-8 init: init

--- apps/spotifun-rtk/src/main.tsx
+++ apps/spotifun-rtk/src/main.tsx
@@ -1,4 +1,4 @@
-import { configureApi, localStorageKeys } from "@it-incubator/spotifun-api-sdk"
+// import { configureApi, localStorageKeys } from "@it-incubator/spotifun-api-sdk"
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
@@ -7,16 +7,16 @@ import { Provider } from "react-redux"
 import { App } from "./app/ui/App.tsx"
 import { store } from "./app/model/store.ts"
 
-configureApi({
-  baseURL: import.meta.env.VITE_BASE_URL!,
-  apiKey: import.meta.env.VITE_API_KEY,
-  getAccessToken: () => localStorage.getItem(localStorageKeys.accessToken),
-  getRefreshToken: () => localStorage.getItem(localStorageKeys.refreshToken),
-  setTokens: (access, refresh) => {
-    localStorage.setItem(localStorageKeys.accessToken, access)
-    localStorage.setItem(localStorageKeys.refreshToken, refresh)
-  },
-})
+// configureApi({
+//   baseURL: import.meta.env.VITE_BASE_URL!,
+//   apiKey: import.meta.env.VITE_API_KEY,
+//   getAccessToken: () => localStorage.getItem(localStorageKeys.accessToken),
+//   getRefreshToken: () => localStorage.getItem(localStorageKeys.refreshToken),
+//   setTokens: (access, refresh) => {
+//     localStorage.setItem(localStorageKeys.accessToken, access)
+//     localStorage.setItem(localStorageKeys.refreshToken, refresh)
+//   },
+// })
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
