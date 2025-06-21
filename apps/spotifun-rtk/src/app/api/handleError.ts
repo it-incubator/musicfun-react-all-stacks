f99c4c56e7ead999f51086ee2429ac423296532c
Natallia <natalyasafarevich@gmail.com>
2025-06-21 10:03:42 +0200

    SP-8 feat: add error handling components

---
+++ apps/spotifun-rtk/src/app/api/handleError.ts
@@ -0,0 +1,24 @@
+import type { BaseQueryApi, FetchBaseQueryError, FetchBaseQueryMeta, QueryReturnValue } from "@reduxjs/toolkit/query"
+import { setError } from "../model/errorSlice"
+
+export const handleError = (
+  api: BaseQueryApi,
+  result: QueryReturnValue<unknown, FetchBaseQueryError, FetchBaseQueryMeta>
+) => {
+  let error = "Some error occurred"
+
+  if (result.error) {
+    const message = (result.error.data as { message: string }).message
+    switch (result.error.status) {
+      case 400:
+      case 500:
+        error = JSON.stringify(message)
+        break
+      default:
+        error = JSON.stringify(message)
+        break
+    }
+    api.dispatch(setError(error))
+  }
+  console.log(result.error)
+}
