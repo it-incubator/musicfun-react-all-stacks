d6f45410b029902fc1e6bfccc29163179904ed96
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-06-22 18:46:02 +0300

    SF-2 feat: playlist logic custom hooks

---
+++ apps/spotifun-rtk/src/features/playlists/lib/hooks/useRemovePlaylist.ts
@@ -0,0 +1,16 @@
+import { useRemovePlaylistMutation } from "@/features/playlists/api/playlistsApi"
+import { showErrorToast } from "@/common/utils"
+
+export const useRemovePlaylist = () => {
+  const [removePlaylistMutation] = useRemovePlaylistMutation()
+
+  const removePlaylist = (playlistId: string) => {
+    if (confirm("Вы уверены, что хотите удалить плейлист?")) {
+      removePlaylistMutation(playlistId)
+        .unwrap()
+        .catch((err: unknown) => showErrorToast("Не удалось удалить плейлист", err))
+    }
+  }
+
+  return { removePlaylist }
+}
