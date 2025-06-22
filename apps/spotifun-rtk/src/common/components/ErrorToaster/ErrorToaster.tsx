33b258f8357de05ee99c3985c94db86e4a16e262
Natallya <59231303+natalyasafarevich@users.noreply.github.com>
2025-06-22 14:22:11 +0200

    Обработка ошибок в Rtk-query (#14)
    * SP-8 init: init
    
    * SP-8 feat: move header
    
    * SP-8 feat: add GlobalLinearProgress
    
    * SP-8 feat: add new api for testing
    
    * SP-8 feat: add error handling components
    
    * SP-8 feat: change toaster component
    
    * SP-8 feat: delete console

---
+++ apps/spotifun-rtk/src/common/components/ErrorToaster/ErrorToaster.tsx
@@ -0,0 +1,16 @@
+import { selectError } from "@/app/model/errorSlice"
+import { useAppSelector } from "@/common/hooks"
+import { useEffect } from "react"
+import { toast, ToastContainer } from "react-toastify/unstyled"
+
+export const ErrorToaster = () => {
+  const selectedError = useAppSelector(selectError)
+
+  useEffect(() => {
+    if (selectedError) {
+      toast.error(selectedError)
+    }
+  }, [selectedError])
+
+  return <ToastContainer newestOnTop={false} closeOnClick={false} rtl={false} pauseOnFocusLoss pauseOnHover />
+}
