3247cb0b91d08288ac10a21c3c7712c207530c80
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-06-22 18:37:31 +0300

    SF-2 feat: add pages and routing for playlists

--- apps/spotifun-rtk/src/common/routing/Routing.tsx
+++ apps/spotifun-rtk/src/common/routing/Routing.tsx
@@ -7,6 +7,7 @@ import { PlaylistPage } from "@/features/playlists/ui/PlaylistPage/PlaylistPage.
 import { TrackPage } from "@/features/tracks/ui/TrackPage/TrackPage.tsx"
 import { TracksPage } from "@/features/tracks/ui/TracksPage/TracksPage.tsx"
 import { OAuthCallback } from "@/features/auth/ui/OAuthRedirect/OAuthCallback.tsx"
+import { PlaylistsPage } from "@/features/playlists/ui/PlaylistsPage/PlaylistsPage.tsx"
 
 export const Path = {
   Main: "/",
@@ -22,13 +23,14 @@ export const Path = {
 export const Routing = () => (
   <Routes>
     <Route path={Path.Main} element={<MainPage />} />
+    <Route path={Path.Playlists} element={<PlaylistsPage />} />
     <Route path={`${Path.Playlists}/:playlistId`} element={<PlaylistPage />} />
     <Route path={Path.Tracks} element={<TracksPage />} />
     <Route path={`${Path.Tracks}/:trackId`} element={<TrackPage />} />
     <Route path={Path.NotFound} element={<PageNotFound />} />
     <Route path={Path.Artists} element={<ArtistsPage />} />
     <Route path={Path.Tags} element={<TagsPage />} />
-    {/*<Route path={Path.Playlists} element={<PlaylistsPage />} />*/}
     <Route path={Path.OAuthRedirect} element={<OAuthCallback />} />
+    {/* <Route path={Path.Auth} element={<AuthPage />} /> */}
   </Routes>
 )
--- apps/spotifun-rtk/src/features/playlists/ui/PlaylistPage/PlaylistPage.tsx
+++ apps/spotifun-rtk/src/features/playlists/ui/PlaylistPage/PlaylistPage.tsx
@@ -1,7 +1,35 @@
+import { Loader, PageTitle } from "@/common/components"
+import { Path } from "@/common/routing"
+import { useFetchPlaylistByIdQuery } from "../../api/playlistsApi"
+import { Link, Navigate, useParams } from "react-router"
+import { PlaylistCover } from "../PlaylistsPage/PlaylistsList/PlaylistItem/PlaylistCover/PlaylistCover"
+import { PlaylistDescription } from "../PlaylistsPage/PlaylistsList/PlaylistItem/PlaylistDescription/PlaylistDescription"
+import { PlaylistTracks } from "./PlaylistTracks/PlaylistTracks"
+
 export const PlaylistPage = () => {
+  const { playlistId } = useParams<{ playlistId?: string }>()
+
+  const { data, isLoading } = useFetchPlaylistByIdQuery(playlistId as string, {
+    skip: !playlistId,
+  })
+
+  if (!playlistId) return <Navigate to={Path.NotFound} />
+
+  if (isLoading) return <Loader />
+
+  if (!data) return <h1>Плейлист не найден</h1>
+
   return (
-    <div>
-      <h1>Страница плейлиста</h1>
-    </div>
+    <>
+      <Link className={"link"} to={Path.Playlists}>
+        Вернуться назад
+      </Link>
+      <PageTitle>Информация о плейлисте</PageTitle>
+      <div>
+        <PlaylistCover playlist={data.data} />
+        <PlaylistDescription attributes={data.data.attributes} />
+      </div>
+      <PlaylistTracks />
+    </>
   )
 }
