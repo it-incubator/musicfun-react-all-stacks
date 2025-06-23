94530a73b743ea4e37b87d54f6a2a95a9d90dd37
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-23 14:19:14 +0200

    refactor: change Loader

--- apps/spotifun-rtk/src/common/components/SearchInput/SearchInput.tsx
+++ apps/spotifun-rtk/src/common/components/SearchInput/SearchInput.tsx
@@ -1,4 +1,5 @@
 import s from "./SearchInput.module.css"
+import { Loader } from "@/common/components"
 
 type Props = {
   search: string
@@ -20,7 +21,11 @@ export const SearchInput = ({ setSearch, search, isPending, title, placeholder, 
         onChange={(e) => setSearch(e.currentTarget.value)}
         autoFocus={autoFocus}
       />
-      {isPending && <span className={s.inputLoader}>Загрузка...</span>}
+      {isPending && (
+        <span className={s.inputLoader}>
+          <Loader />
+        </span>
+      )}
     </div>
   </div>
 )
