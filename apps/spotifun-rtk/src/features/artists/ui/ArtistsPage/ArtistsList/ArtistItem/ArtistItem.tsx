996f39d4f8f5d5b64fb3d7fd93f18916567a3099
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-23 14:22:28 +0200

    feat: add delete handler

--- apps/spotifun-rtk/src/features/artists/ui/ArtistsPage/ArtistsList/ArtistItem/ArtistItem.tsx
+++ apps/spotifun-rtk/src/features/artists/ui/ArtistsPage/ArtistsList/ArtistItem/ArtistItem.tsx
@@ -1,24 +1,30 @@
 import s from "./ArtistItem.module.css"
-import { type Artist } from "@it-incubator/spotifun-api-sdk"
 import { useDeleteArtistMutation } from "@/features/artists/api/artistsApi.ts"
+import type { Artist } from "@/features/artists/api/artistsApi.types.ts"
+import { useAppDispatch } from "@/common/hooks"
+import { setError } from "@/app/model/errorSlice.ts"
 
 type Props = {
   artist: Artist
 }
 
 export const ArtistItem = ({ artist }: Props) => {
+  const dispatch = useAppDispatch()
   const { name, id } = artist
   const [deleteArtist, { isLoading }] = useDeleteArtistMutation()
+  const deleteHandler = () => {
+    deleteArtist(id)
+      .unwrap()
+      .catch((error) => {
+        dispatch(setError(error.data.message))
+      })
+  }
   return (
     <div className={`${s.item} ${s.itemFullwidth} ${s.flexContainer} ${s.container}`}>
       <div>
         <b>Name:</b> <span>{name}</span>
       </div>
-      <button
-        onClick={() => {
-          deleteArtist(id)
-        }}
-      >
+      <button onClick={deleteHandler} disabled={isLoading}>
         {isLoading ? "Удаление..." : "Удалить"}
       </button>
     </div>
