a46abcd080836870e62b43d23305970f38b4b156
Tatiana Kostecki, MA <131677870+tanya-kostecki@users.noreply.github.com>
2025-06-24 08:37:25 +0200

    SF-19 init: add api for tags page (#20)

--- apps/spotifun-rtk/src/features/tags/ui/TagsPage/TagsList/TagItem/TagItem.tsx
+++ apps/spotifun-rtk/src/features/tags/ui/TagsPage/TagsList/TagItem/TagItem.tsx
@@ -1,8 +1,5 @@
-import { tagsKey } from "@/common/apiEntities"
-import { tagsApi } from "../../../../api/tagsApi.ts"
+import { useRemoveTagMutation } from "../../../../api/tagsApi.ts"
 import type { Tag } from "../../../../api/tagsApi.types.ts"
-import { queryClient } from "@/main.tsx"
-import { useMutation } from "@tanstack/react-query"
 import s from "./TagItem.module.css"
 
 type Props = {
@@ -12,17 +9,14 @@ type Props = {
 export const TagItem = ({ tag }: Props) => {
   const { name, id } = tag
 
-  const { mutate, isPending: isRemoving } = useMutation({
-    mutationFn: tagsApi.removeTag,
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: [tagsKey] }),
-  })
+  const [removeTag, { isLoading: isRemoving }] = useRemoveTagMutation()
 
   return (
     <div className={`item item--fullwidth flex-container ${s.container}`}>
       <div>
         <b>Tag:</b> <span>{name}</span>
       </div>
-      <button onClick={() => mutate(id)} disabled={isRemoving}>
+      <button onClick={() => removeTag({ id })} disabled={isRemoving}>
         {isRemoving ? "Удаление..." : "Удалить"}
       </button>
     </div>
