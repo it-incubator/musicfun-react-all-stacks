e2dd2dca9039026bdb67a3243d20374d3dfc8b5f
Artem Rybin <124868063+catobazz@users.noreply.github.com>
2025-06-24 17:23:16 +0300

    SF-17 feat: add user description

--- apps/spotifun-rtk/src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistItem/PlaylistDescription/PlaylistDescription.tsx
+++ apps/spotifun-rtk/src/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistItem/PlaylistDescription/PlaylistDescription.tsx
@@ -1,28 +1,31 @@
-import type { PlaylistAttributes } from "../../../../../api/playlistsApi.types"
+import type {PlaylistAttributes} from "../../../../../api/playlistsApi.types"
 
 type Props = {
-  attributes: PlaylistAttributes
+    attributes: PlaylistAttributes
 }
 
-export const PlaylistDescription = ({ attributes }: Props) => {
-  const { title, description, tags, addedAt, order } = attributes
-  return (
-    <>
-      <div>
-        <b>title:</b> <span>{title}</span>
-      </div>
-      <div>
-        <b>description:</b> <span>{description || "Описание не добавлено"}</span>
-      </div>
-      <div>
-        <b>order:</b> <span>{order}</span>
-      </div>
-      <div>
-        <b>tags:</b> <span>{tags.length ? tags.map((t) => t) : "Теги не добавлены"}</span>
-      </div>
-      <div>
-        <b>added date:</b> <span>{new Date(addedAt).toLocaleDateString()}</span>
-      </div>
-    </>
-  )
+export const PlaylistDescription = ({attributes}: Props) => {
+    const {title, description, tags, addedAt, order, user} = attributes
+    return (
+        <>
+            <div>
+                <b>title:</b> <span>{title}</span>
+            </div>
+            <div>
+                <b>description:</b> <span>{description || "Описание не добавлено"}</span>
+            </div>
+            <div>
+                <b>order:</b> <span>{order}</span>
+            </div>
+            <div>
+                <b>tags:</b> <span>{tags.length ? tags.map((t) => t) : "Теги не добавлены"}</span>
+            </div>
+            <div>
+                <b>added date:</b> <span>{new Date(addedAt).toLocaleDateString()}</span>
+            </div>
+            <div>
+                <b>author:</b>{" "} <span>name:{user.name} : id- {user.id}</span>
+            </div>
+        </>
+    )
 }
