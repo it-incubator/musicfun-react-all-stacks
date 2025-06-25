933162ad5bfd1304ff4e0c672b7845214d9d8ad4
Natallia <natalyasafarevich@gmail.com>
2025-06-25 16:10:36 +0200

    SP-8-FIX refactor: format errorHandler

--- apps/spotifun-rtk/src/common/utils/errorHandler.ts
+++ apps/spotifun-rtk/src/common/utils/errorHandler.ts
@@ -1,24 +1,21 @@
-import type { FieldValues, UseFormSetError, Path } from "react-hook-form"
+import type {FieldValues, UseFormSetError, Path} from 'react-hook-form';
 
 type ErrorWithKey = {
-	data?: {
-		extensions?: { key?: string, message?: string }[]
-	}
-}
+  data?: {
+    extensions?: {key?: string; message?: string}[];
+  };
+};
 
-export const errorHandler = <T extends FieldValues>(
-	e: unknown,
-	setError: UseFormSetError<T>
-) => {
-	const error = e as ErrorWithKey
-	
-	const key = error?.data?.extensions?.[0]?.key
-	const message = error?.data?.extensions?.[0]?.message
+export const errorHandler = <T extends FieldValues>(e: unknown, setError: UseFormSetError<T>) => {
+  const error = e as ErrorWithKey;
 
-	if (key) {
-		setError(key as Path<T>, {
-			type: "manual",
-			message,
-		})
-	}
-}
+  const key = error?.data?.extensions?.[0]?.key;
+
+  const message = error?.data?.extensions?.[0]?.message;
+  if (key) {
+    setError(key as Path<T>, {
+      type: 'manual',
+      message,
+    });
+  }
+};
