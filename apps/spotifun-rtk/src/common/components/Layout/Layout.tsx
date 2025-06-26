41f6a912032d41d5161a6515d957d48f2c0ccd8a
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-26 16:39:02 +0200

    feat: add navBar component

--- apps/spotifun-rtk/src/common/components/Layout/Layout.tsx
+++ apps/spotifun-rtk/src/common/components/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import type { ReactNode } from "react"
 import s from "./Layout.module.css"
+import { NavBar } from "@/common/components/NavBar/NavBar.tsx"
+
 
 type Props = {
   children: ReactNode
@@ -8,6 +10,7 @@ type Props = {
 export const Layout = ({ children }: Props) => {
   return (
     <div className={s.layout}>
+      <NavBar/>
       <div className={s.layoutContent}>{children}</div>
     </div>
   )
