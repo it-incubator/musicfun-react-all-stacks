698a6c048503ede0540394a0ff84f49118f255dd
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-26 16:40:21 +0200

    feat: add playlists and tracks to the main page

--- apps/spotifun-rtk/src/app/ui/MainPage.tsx
+++ apps/spotifun-rtk/src/app/ui/MainPage.tsx
@@ -1,11 +1,26 @@
+import { useFetchTracksQuery } from "@/features/tracks/api/tracksApi.ts"
+import { useFetchPlaylistsQuery } from "@/features/playlists/api/playlistsApi.ts"
+import { TrackItem } from "@/features/tracks/ui/TracksPage/TracksList/TrackItem/TrackItem.tsx"
+import { PlaylistsList } from "@/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx"
+import s from './MainPage.module.css'
+
 export const MainPage = () => {
+  const { data: playlists } = useFetchPlaylistsQuery({ search: "", pageNumber: 1, pageSize: 10 })
+  const { data: tracks } = useFetchTracksQuery({ search: "", pageNumber: 1, pageSize: 10 })
   return (
     <div>
       <h1>Главная страница</h1>
       <div>Good afternoon</div>
       <div>Your top mixes</div>
       <div>Made for you</div>
-      <div>...</div>
+      <div>
+        <h2>New playlists</h2>
+        {playlists && <PlaylistsList playlists={playlists.data} />}
+      </div>
+      <div className={s.container}>
+        <h2>New tracks</h2>
+        {tracks && tracks.data.map((track, i) => <TrackItem track={track} key={i} />)}
+      </div>
     </div>
   )
 }
