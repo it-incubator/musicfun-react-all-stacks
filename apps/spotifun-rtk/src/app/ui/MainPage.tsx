aef06d72caad063d9b12269513610ba30da6488a
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-06-27 15:04:36 +0200

    refactor: add default params

--- apps/spotifun-rtk/src/app/ui/MainPage.tsx
+++ apps/spotifun-rtk/src/app/ui/MainPage.tsx
@@ -2,24 +2,24 @@ import { useFetchTracksQuery } from "@/features/tracks/api/tracksApi.ts"
 import { useFetchPlaylistsQuery } from "@/features/playlists/api/playlistsApi.ts"
 import { TrackItem } from "@/features/tracks/ui/TracksPage/TracksList/TrackItem/TrackItem.tsx"
 import { PlaylistsList } from "@/features/playlists/ui/PlaylistsPage/PlaylistsList/PlaylistsList.tsx"
-import s from './MainPage.module.css'
+import s from "./MainPage.module.css"
 
 export const MainPage = () => {
-  const { data: playlists } = useFetchPlaylistsQuery({ search: "", pageNumber: 1, pageSize: 10 })
-  const { data: tracks } = useFetchTracksQuery({ search: "", pageNumber: 1, pageSize: 10 })
+  const defaultParams = { search: "", pageSize: 10, pageNumber: 1 }
+
+  const { data: playlists } = useFetchPlaylistsQuery(defaultParams)
+  const { data: tracks } = useFetchTracksQuery(defaultParams)
+
   return (
     <div>
       <h1>Главная страница</h1>
-      <div>Good afternoon</div>
-      <div>Your top mixes</div>
-      <div>Made for you</div>
       <div>
         <h2>New playlists</h2>
         {playlists && <PlaylistsList playlists={playlists.data} />}
       </div>
       <div className={s.container}>
         <h2>New tracks</h2>
-        {tracks && tracks.data.map((track, i) => <TrackItem track={track} key={i} />)}
+        {tracks && tracks.data.map((track) => <TrackItem track={track} key={track.id} />)}
       </div>
     </div>
   )
