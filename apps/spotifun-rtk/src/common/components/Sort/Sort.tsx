93de5e3e498e9dbd04b7814469457b4fd74b0497
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-07-04 12:12:23 +0200

    refactor: sort by direction add values

--- apps/spotifun-rtk/src/common/components/Sort/Sort.tsx
+++ apps/spotifun-rtk/src/common/components/Sort/Sort.tsx
@@ -1,24 +1,40 @@
 import type { ChangeEvent } from "react"
 
 type Props = {
-  direction: "new" | "old" | "topRate"
-  setSortDirection: (direction: "new" | "old" | "topRate") => void
+  sortDirection: "asc" | "desc"
+  sortBy: "addedAt" | "likesCount"
+  setSortBy: (direction: "addedAt" | "likesCount") => void
+  setSortDirection: (direction: "asc" | "desc") => void
 }
-export const Sort = ({ setSortDirection, direction }: Props) => {
+export const Sort = ({ setSortDirection, setSortBy, sortDirection, sortBy }: Props) => {
   const onChangeHandler = (e: ChangeEvent<HTMLSelectElement>) => {
     const value = e.target.value
-    if (value === "new" || value === "old" || value === "topRate") {
-      setSortDirection(value)
+    switch (value) {
+      case "asc":
+        setSortDirection("asc")
+        break
+      case "desc":
+        setSortDirection("desc")
+        break
+      case "likesCount":
+        setSortBy("likesCount")
+        setSortDirection("desc")
+        break
+      default:
+        setSortBy("addedAt")
+        setSortDirection("desc")
     }
   }
+
+  const value = sortBy === "likesCount" ? sortBy : sortDirection
 
   return (
     <div>
       <label>Sorted by </label>
-      <select value={direction} onChange={onChangeHandler}>
-        <option value={"new"}>Newest first</option>
-        <option value={"old"}>Oldest first</option>
-        <option value={"topRate"}>Top-rated first</option>
+      <select value={value} onChange={onChangeHandler}>
+        <option value={"desc"}>Newest first</option>
+        <option value={"asc"}>Oldest first</option>
+        <option value={"likesCount"}>Top-rated first</option>
       </select>
     </div>
   )
