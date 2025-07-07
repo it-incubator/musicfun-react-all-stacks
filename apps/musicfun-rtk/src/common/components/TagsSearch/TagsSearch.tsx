c3fce31b6705739367bf3c0d30926e0aea2eef74
Arina-Vas <mrs.a.vasilevskaya@gmail.com>
2025-07-07 18:41:17 +0200

    refactor: tags search styles

---
+++ apps/musicfun-rtk/src/common/components/TagsSearch/TagsSearch.tsx
@@ -0,0 +1,33 @@
+import { useFindTagsQuery } from '@/features/tags/api/tagsApi.ts'
+import { useState } from 'react'
+import { useDebounceValue } from '@/common/hooks'
+import s from './TagsSearch.module.css'
+import { AutoComplete } from '@/common/components/AutoComplete/AutoComplete.tsx'
+
+type Props = {
+  setValues: (tags: string[]) => void
+  selectedIds: string[]
+}
+
+export const TagsSearch = ({ setValues, selectedIds }: Props) => {
+  const [search, setSearch] = useState('')
+  const [debouncedSearch] = useDebounceValue(search)
+  const { data, isLoading } = useFindTagsQuery({ value: debouncedSearch })
+
+  const tags = data || []
+
+  return (
+    <div className={s.container}>
+      <h2>Поиск по тегу</h2>
+      <AutoComplete
+        placeholder={'Choose tag'}
+        items={tags}
+        search={search}
+        setSearch={setSearch}
+        isPending={isLoading}
+        setValues={setValues}
+        selectedIds={selectedIds}
+      />
+    </div>
+  )
+}
