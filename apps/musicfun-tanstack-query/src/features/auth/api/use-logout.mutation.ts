d4002712367dd3d04764c7d8568803aeb5e7c6a1
KirillDolzhenkov <k.dolzhenkov@gmail.com>
2025-07-13 17:40:40 +0300

    refactor: little fixes

--- apps/musicfun-tanstack-query/src/features/auth/api/use-logout.mutation.ts
+++ apps/musicfun-tanstack-query/src/features/auth/api/use-logout.mutation.ts
@@ -12,14 +12,13 @@ export const useLogoutMutation = () => {
       return requestWrapper(
         getClient().POST('/auth/logout', {
           body: {
-            refreshToken: localStorage.getItem(localStorageKeys.refreshToken) || '',
+            refreshToken: localStorage.getItem(localStorageKeys.refreshToken)!,
           },
         })
       )
     },
     onSuccess: async () => {
-      // Согласно API документации, access-токен остается валидным после logout
-      // Удаляем только refresh token, который был деактивирован на сервере
+      localStorage.removeItem(localStorageKeys.accessToken)
       localStorage.removeItem(localStorageKeys.refreshToken)
       await qc.resetQueries({ queryKey: ['auth', 'me'] }) // resetQueries переводит query в изначальное состояние и уведомляет подписчиков — компонент получит data = undefined.
       //await qc.invalidateQueries({ queryKey: ['auth', 'me'] }) // invalidateQueries заставит его немедленно перефетчиться без токена ⇒ получите 401 ⇒ data станет undefined / error.
