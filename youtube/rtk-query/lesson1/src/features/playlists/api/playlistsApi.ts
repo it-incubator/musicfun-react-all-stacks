3c9e24514184581394b6125562b69c77d4ad7fdc
safronman <safronmanbox@gmail.com>
2025-08-08 10:55:25 +0300

    🔶 12. Optimistic update

--- youtube/rtk-query/lesson1/src/features/playlists/api/playlistsApi.ts
+++ youtube/rtk-query/lesson1/src/features/playlists/api/playlistsApi.ts
@@ -23,7 +23,43 @@ export const playlistsApi = baseApi.injectEndpoints({
       invalidatesTags: ['Playlist'],
     }),
     updatePlaylist: build.mutation<void, { playlistId: string; body: UpdatePlaylistArgs }>({
-      query: ({ playlistId, body }) => ({ method: 'put', url: `playlists/${playlistId}`, body }),
+      query: ({ playlistId, body }) => {
+        return { method: 'put', url: `playlists/${playlistId}`, body }
+      },
+      onQueryStarted: async ({ playlistId, body }, { queryFulfilled, dispatch, getState }) => {
+        const args = playlistsApi.util.selectCachedArgsForQuery(getState(), 'fetchPlaylists')
+
+        const patchCollections: any[] = []
+
+        args.forEach((arg) => {
+          patchCollections.push(
+            dispatch(
+              playlistsApi.util.updateQueryData(
+                'fetchPlaylists',
+                {
+                  pageNumber: arg.pageNumber,
+                  pageSize: arg.pageSize,
+                  search: arg.search,
+                },
+                (state) => {
+                  const index = state.data.findIndex((playlist) => playlist.id === playlistId)
+                  if (index !== -1) {
+                    state.data[index].attributes = { ...state.data[index].attributes, ...body }
+                  }
+                },
+              ),
+            ),
+          )
+        })
+
+        try {
+          await queryFulfilled
+        } catch (e) {
+          patchCollections.forEach((patchCollection) => {
+            patchCollection.undo()
+          })
+        }
+      },
       invalidatesTags: ['Playlist'],
     }),
     uploadPlaylistCover: build.mutation<Images, { playlistId: string; file: File }>({
