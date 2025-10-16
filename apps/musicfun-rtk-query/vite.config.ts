f0fa975914cf14b044ece7355148ec58a79f8eff
dimych <kuzyuberdin@gmail.com>
2025-10-16 21:07:01 +0400

    try deploy with subfolders

--- apps/musicfun-rtk-query/vite.config.ts
+++ apps/musicfun-rtk-query/vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/rtkquery/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
--- apps/musicfun-tanstack-query/vite.config.ts
+++ apps/musicfun-tanstack-query/vite.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/tanstackquery/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
