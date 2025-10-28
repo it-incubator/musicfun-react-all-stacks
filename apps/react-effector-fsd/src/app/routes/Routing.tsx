3536cbfe6c428aebd61852c4561235f0ad6de785
Muzyk0 <muzyk0@proton.me>
2025-10-28 21:32:11 +0300

    feat(auth): Add OAuth redirect

--- apps/react-effector-fsd/src/app/routes/Routing.tsx
+++ apps/react-effector-fsd/src/app/routes/Routing.tsx
@@ -3,9 +3,11 @@ import { Route, Routes } from 'react-router'
 import { Layout } from '@/widgets/layout'
 import { UserPage } from '@/pages/user'
 import { Home } from '@/pages/home'
+import { OAuthCallback } from '@/pages/auth/OAuthRedirect/OAuthCallback.tsx'
 
 export const Routing = () => (
   <Routes>
+    <Route path="/oauth/callback" element={<OAuthCallback />} />
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
 
