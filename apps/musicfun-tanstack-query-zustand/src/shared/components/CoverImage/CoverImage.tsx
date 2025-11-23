4bfb89e6a53f255f29f3ac38b506c84e668ba1a4
Kirill Dolzhenkov <k.dolzhenkov@gmail.com>
2025-11-23 08:57:38 +0300

    fix: fixed CoverImage styles

--- apps/musicfun-tanstack-query-zustand/src/shared/components/CoverImage/CoverImage.tsx
+++ apps/musicfun-tanstack-query-zustand/src/shared/components/CoverImage/CoverImage.tsx
@@ -2,11 +2,17 @@ import type { ComponentProps } from 'react'
 
 import noCoverPlaceholder from '@/assets/img/no-cover.png'
 
+import s from './CoverImage.styles.module.scss'
+
 type Props = {
   imageSrc?: string | undefined
   imageDescription: string
 } & ComponentProps<'img'>
 
 export const CoverImage = ({ imageSrc, imageDescription, ...props }: Props) => {
-  return <img src={imageSrc ? imageSrc : noCoverPlaceholder} alt={imageDescription} {...props} />
+  return (
+    <div className={s.coverImageWrapper}>
+      <img src={imageSrc ? imageSrc : noCoverPlaceholder} alt={imageDescription} {...props} />
+    </div>
+  )
 }
