603600bb695464fe95e8ee4183c97eb808fe2997
AnatolyPautovDev <pautov.tolick@yandex.by>
2025-12-15 12:29:26 +0300

    chore: add constants module and move shared variable

--- apps/rtk-query/src/shared/utils/setLocale.ts
+++ apps/rtk-query/src/shared/utils/setLocale.ts
@@ -1,4 +1,5 @@
 import i18n from 'i18next'
+import { LOCALE_KEY } from '@/shared/constants'
 
 export type Locale = 'en' | 'ru'
 
@@ -11,5 +12,5 @@ export type Locale = 'en' | 'ru'
  */
 export const setLocale = (lng: Locale): void => {
   void i18n.changeLanguage(lng)
-  localStorage.setItem('locale', lng)
+  localStorage.setItem(LOCALE_KEY, lng)
 }
