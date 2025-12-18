056b114ab796a27cea989018ed3991bd89193daf
nataliaN <asdfghmax@yandex.ru>
2025-12-18 16:31:52 +0300

    164 display correct tabs on another user's profile (#195)
    * refactor: display correct tabs on another user's profile:
    
    * refactor: display correct tabs on user's profile

--- apps/rtk-query/src/pages/UserPage/ui/UserInfo/UserInfo.tsx
+++ apps/rtk-query/src/pages/UserPage/ui/UserInfo/UserInfo.tsx
@@ -1,47 +1,47 @@
-import { useTranslation } from 'react-i18next'
-
-import { setIsAuthModalOpen, useMeQuery } from '@/features/auth'
-import { selectProfileAvatar, selectProfileFullName } from '@/features/profile'
-import { useEditProfileModal } from '@/features/profile'
-import { Avatar, Button, Typography } from '@/shared/components'
-import { useAppDispatch, useAppSelector } from '@/shared/hooks'
-import { EditIcon } from '@/shared/icons'
+import {useTranslation} from 'react-i18next'
+import {selectProfileAvatar, selectProfileFullName, useEditProfileModal} from '@/features/profile'
+import {Avatar, Button, Typography} from '@/shared/components'
+import {useAppSelector} from '@/shared/hooks'
+import {EditIcon} from '@/shared/icons'
 
 import s from './UserInfo.module.css'
+import {useOwnerData} from "@/pages/UserPage/hooks";
 
 export const UserInfo = () => {
-  const { t } = useTranslation()
+    const {t} = useTranslation()
 
-  const dispatch = useAppDispatch()
-  const { data } = useMeQuery()
+    const {isProfileOwner, userLogin} = useOwnerData()
 
-  const { handleOpenEditProfileModal } = useEditProfileModal()
-  const profileAvatarUrl = useAppSelector(selectProfileAvatar)
-  const profileFullName = useAppSelector(selectProfileFullName)
+    const {handleOpenEditProfileModal} = useEditProfileModal()
+    const profileAvatarUrl = useAppSelector(selectProfileAvatar)
+    const profileFullName = useAppSelector(selectProfileFullName)
 
-  const handleOpenAuthModal = () => {
-    dispatch(setIsAuthModalOpen({ isAuthModalOpen: true }))
-  }
+    let userFullName
+    if (isProfileOwner && profileFullName.name) {
+        userFullName = `${profileFullName.name} ${profileFullName.surname}`
+    } else {
+        userFullName = userLogin
+    }
 
-  const isAuth = !!data
+    return (
+        <div className={s.box}>
+            <Avatar src={profileAvatarUrl}
+                    fullName={isProfileOwner ? profileFullName : undefined}
+                    userLogin={userLogin}/>
+            <Typography variant="h2">
+                {userFullName}
+            </Typography>
+            {isProfileOwner && <Button
+                className={s.editButton}
+                variant="secondary"
+                onClick={handleOpenEditProfileModal}>
+                <EditIcon/>
+                {t('button.edit_profile')}
+            </Button>}
 
-  return (
-    <div className={s.box}>
-      <Avatar src={profileAvatarUrl} fullName={profileFullName} userLogin={data?.login} />
-      <Typography variant="h2">
-        {profileFullName?.name ? `${profileFullName.name} ${profileFullName.surname}` : data?.login}
-      </Typography>
-      <Button
-        className={s.editButton}
-        variant="secondary"
-        onClick={isAuth ? handleOpenEditProfileModal : handleOpenAuthModal}>
-        <EditIcon />
-        {t('button.edit_profile')}
-      </Button>
+            {/* TODO: Backend don't return this data 😢 */}
 
-      {/* TODO: Backend don't return this data 😢 */}
-
-      {/* <dl className={s.descriptionList}>
+            {/* <dl className={s.descriptionList}>
         <div className={s.descriptionItem}>
           <Typography as="dd" variant="body1">
             58
@@ -59,6 +59,6 @@ export const UserInfo = () => {
           </Typography>
         </div>
       </dl> */}
-    </div>
-  )
+        </div>
+    )
 }
