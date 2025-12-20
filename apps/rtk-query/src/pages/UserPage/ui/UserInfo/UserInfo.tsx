80add4435ab6f877cbc58a40eee2992f0c4d1934
molnia_grom <mgrom250@gmail.com>
2025-12-20 08:22:05 +0200

    fix: Due to a minor error, the application crashed, but it has been fixed.

--- apps/rtk-query/src/pages/UserPage/ui/UserInfo/UserInfo.tsx
+++ apps/rtk-query/src/pages/UserPage/ui/UserInfo/UserInfo.tsx
@@ -1,47 +1,46 @@
-import {useTranslation} from 'react-i18next'
-import {selectProfileAvatar, selectProfileFullName, useEditProfileModal} from '@/features/profile'
-import {Avatar, Button, Typography} from '@/shared/components'
-import {useAppSelector} from '@/shared/hooks'
-import {EditIcon} from '@/shared/icons'
+import { useTranslation } from 'react-i18next'
+import { selectProfileAvatar, selectProfileFullName, useEditProfileModal } from '@/features/profile'
+import { Avatar, Button, Typography } from '@/shared/components'
+import { useAppSelector } from '@/shared/hooks'
+import { EditIcon } from '@/shared/icons'
 
 import s from './UserInfo.module.css'
-import {useOwnerData} from "@/pages/UserPage/hooks";
+import { useOwnerData } from '@/pages/UserPage/hooks'
 
 export const UserInfo = () => {
-    const {t} = useTranslation()
+  const { t } = useTranslation()
 
-    const {isProfileOwner, userLogin} = useOwnerData()
+  const { isProfileOwner, userLogin } = useOwnerData()
 
-    const {handleOpenEditProfileModal} = useEditProfileModal()
-    const profileAvatarUrl = useAppSelector(selectProfileAvatar)
-    const profileFullName = useAppSelector(selectProfileFullName)
+  const { handleOpenEditProfileModal } = useEditProfileModal()
+  const profileAvatarUrl = useAppSelector(selectProfileAvatar)
+  const profileFullName = useAppSelector(selectProfileFullName)
 
-    let userFullName
-    if (isProfileOwner && profileFullName.name) {
-        userFullName = `${profileFullName.name} ${profileFullName.surname}`
-    } else {
-        userFullName = userLogin
-    }
+  let userFullName
+  if (isProfileOwner && profileFullName.name) {
+    userFullName = `${profileFullName.name} ${profileFullName.surname}`
+  } else {
+    userFullName = userLogin
+  }
 
-    return (
-        <div className={s.box}>
-            <Avatar src={profileAvatarUrl}
-                    fullName={isProfileOwner ? profileFullName : undefined}
-                    userLogin={userLogin}/>
-            <Typography variant="h2">
-                {userFullName}
-            </Typography>
-            {isProfileOwner && <Button
-                className={s.editButton}
-                variant="secondary"
-                onClick={handleOpenEditProfileModal}>
-                <EditIcon/>
-                {t('button.edit_profile')}
-            </Button>}
+  return (
+    <div className={s.box}>
+      <Avatar
+        src={profileAvatarUrl}
+        fullName={isProfileOwner ? profileFullName : undefined}
+        userLogin={userLogin}
+      />
+      <Typography variant="h2">{userFullName}</Typography>
+      {isProfileOwner && (
+        <Button className={s.editButton} variant="secondary" onClick={handleOpenEditProfileModal}>
+          <EditIcon />
+          {t('button.edit_profile')}
+        </Button>
+      )}
 
-            {/* TODO: Backend don't return this data 😢 */}
+      {/* TODO: Backend don't return this data 😢 */}
 
-            {/* <dl className={s.descriptionList}>
+      {/* <dl className={s.descriptionList}>
         <div className={s.descriptionItem}>
           <Typography as="dd" variant="body1">
             58
@@ -59,6 +58,6 @@ export const UserInfo = () => {
           </Typography>
         </div>
       </dl> */}
-        </div>
-    )
+    </div>
+  )
 }
--- apps/rtk-query/src/pages/UserPage/ui/UserTabs/PlaylistsTab/PlaylistsTab.tsx
+++ apps/rtk-query/src/pages/UserPage/ui/UserTabs/PlaylistsTab/PlaylistsTab.tsx
@@ -1,72 +1,80 @@
-import {useTranslation} from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 import {
-    PlaylistCard,
-    useCreatePlaylistModal,
-    useEditPlaylistModal,
-    useRemovePlaylistMutation,
+  PlaylistCard,
+  useCreatePlaylistModal,
+  useEditPlaylistModal,
+  useRemovePlaylistMutation,
 } from '@/features/playlists'
-import {ContentList} from '@/pages/common'
-import {Button, DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from '@/shared/components'
-import {MoreIcon} from '@/shared/icons'
-import {ImageType} from '@/shared/types/commonApi.types'
-import {getImageByType} from '@/shared/utils'
+import { ContentList } from '@/pages/common'
+import {
+  Button,
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/shared/components'
+import { MoreIcon } from '@/shared/icons'
+import { ImageType } from '@/shared/types/commonApi.types'
+import { getImageByType } from '@/shared/utils'
 
 import s from './PlaylistsTab.module.css'
-import {useOwnerData} from "@/pages/UserPage/hooks";
+import { useOwnerData } from '@/pages/UserPage/hooks'
 
 export const PlaylistsTab = () => {
-    const {t} = useTranslation()
-    const {isProfileOwner, playlists} = useOwnerData()
+  const { t } = useTranslation()
+  const { isProfileOwner, playlists } = useOwnerData()
 
-    const {handleOpenCreatePlaylistModal} = useCreatePlaylistModal()
-    const {handleOpenEditPlaylistModal} = useEditPlaylistModal()
-    const [removePlaylist] = useRemovePlaylistMutation()
+  const { handleOpenCreatePlaylistModal } = useCreatePlaylistModal()
+  const { handleOpenEditPlaylistModal } = useEditPlaylistModal()
+  const [removePlaylist] = useRemovePlaylistMutation()
 
-    return (
-        <>
-            {isProfileOwner && <Button className={s.createPlaylistButton} onClick={handleOpenCreatePlaylistModal}>
-                {t('playlists.button.create_playlist')}
-            </Button>}
+  return (
+    <>
+      {isProfileOwner && (
+        <Button className={s.createPlaylistButton} onClick={handleOpenCreatePlaylistModal}>
+          {t('playlists.button.create_playlist')}
+        </Button>
+      )}
 
-            {playlists?.data && (
-                <ContentList
-                    data={playlists?.data}
-                    renderItem={(playlist) => {
-                        const image = getImageByType(playlist.attributes.images, ImageType.MEDIUM)
-                        return (
-                            <PlaylistCard
-                                id={playlist.id}
-                                title={playlist.attributes.title}
-                                imageSrc={image?.url}
-
-
-                                actions={
-                                    isProfileOwner && <DropdownMenu>
-                                        <DropdownMenuTrigger>
-                                            <MoreIcon/>
-                                        </DropdownMenuTrigger>
-                                        <DropdownMenuContent>
-                                            <DropdownMenuItem
-                                                onClick={() => {
-                                                    handleOpenEditPlaylistModal(playlist.id)
-                                                }}>
-                                                {t('button.edit')}
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem
-                                                onClick={() => {
-                                                    removePlaylist(playlist.id)
-                                                }}>
-                                                {t('button.delete')}
-                                            </DropdownMenuItem>
-                                        </DropdownMenuContent>
-                                    </DropdownMenu>
-                                }
-                            />
-                        )
-                    }}
-                />
-            )}
-        </>
-    )
+      {playlists?.data && (
+        <ContentList
+          data={playlists?.data}
+          renderItem={(playlist) => {
+            const image = getImageByType(playlist.attributes.images, ImageType.MEDIUM)
+            return (
+              <PlaylistCard
+                id={playlist.id}
+                title={playlist.attributes.title}
+                imageSrc={image?.url}
+                actions={
+                  isProfileOwner && (
+                    <DropdownMenu>
+                      <DropdownMenuTrigger>
+                        <MoreIcon />
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            handleOpenEditPlaylistModal(playlist.id)
+                          }}>
+                          {t('button.edit')}
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                          onClick={() => {
+                            removePlaylist(playlist.id)
+                          }}>
+                          {t('button.delete')}
+                        </DropdownMenuItem>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  )
+                }
+              />
+            )
+          }}
+        />
+      )}
+    </>
+  )
 }
