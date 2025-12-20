0ab16130f3e1883010fb10d61d8feeeacd62862d
molnia_grom <mgrom250@gmail.com>
2025-12-20 17:47:48 +0200

    refactor: delete lines of code are commented

--- apps/rtk-query/src/shared/hooks/useGlobalLoading.ts
+++ apps/rtk-query/src/shared/hooks/useGlobalLoading.ts
@@ -4,12 +4,6 @@ export const useIsGlobalLoading = () => {
   return useAppSelector((state) => {
     const queries = state.baseApi.queries
     const mutations = state.baseApi.mutations
-
-    // Filter requests, excluding requests to download tracks but for some reason it doesn't work
-    // const nonTrackQueries = Object.entries(queries).filter(
-    //   ([key]) => !key.includes('fetchTracksInfinite')
-    // )
-
     const isLoadingQueries = Object.values(queries).some((q) => q?.status === 'pending')
     const isLoadingMutations = Object.values(mutations).some((m) => m?.status === 'pending')
 
