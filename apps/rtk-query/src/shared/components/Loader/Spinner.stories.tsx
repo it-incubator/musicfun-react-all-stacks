853b45f5ac194fc6b047735d7124c7dacba41027
molnia_grom <mgrom250@gmail.com>
2025-12-20 17:37:31 +0200

    feat(spinner): add Storybook for Spinner component

---
+++ apps/rtk-query/src/shared/components/Loader/Spinner.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from '@storybook/react-vite'
+
+import { Spinner } from './Spinner'
+
+const meta = {
+  argTypes: {
+    size: {
+      control: { type: 'number' },
+    },
+    fullScreen: {
+      control: { type: 'boolean' },
+    },
+  },
+  component: Spinner,
+  tags: ['autodocs'],
+  title: 'Components/Spinner',
+} satisfies Meta<typeof Spinner>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {
+    fullScreen: false,
+    size: 48,
+  },
+}
+
+export const FullScreen: Story = {
+  args: {
+    fullScreen: true,
+    size: 100,
+  },
+}
