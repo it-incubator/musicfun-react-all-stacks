7e80194f18a55878a15f07bb38e09cc4acdc088e
Svyatoslav <68824715+Svyat-1418@users.noreply.github.com>
2025-12-24 20:08:44 +0300

    fix: make a fix for the #212 issue (#213)

--- apps/rtk-query/src/layout/AppLoader/AppLoader.tsx
+++ apps/rtk-query/src/layout/AppLoader/AppLoader.tsx
@@ -1,11 +1,8 @@
 import { Loader } from '@/shared/components'
 import { useIsGlobalLoading } from '@/shared/hooks'
-import { useSelector } from 'react-redux'
-import type { RootState } from '@/app/store/store'
 
 export const AppLoader = () => {
   const isLoading = useIsGlobalLoading()
-  const isAdditionalLoading = useSelector((state: RootState) => state.additionalLoading)
-  if (!isLoading || isAdditionalLoading) return null
+  if (!isLoading) return null
   return <Loader />
 }
