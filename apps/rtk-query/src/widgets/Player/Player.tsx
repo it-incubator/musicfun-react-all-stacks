7078c04d3bbef1b3b1723ec9b42f0a57e6874620
molnia_grom <mgrom250@gmail.com>
2025-12-25 14:37:24 +0200

    fix: fix loading title cover artist

--- apps/rtk-query/src/widgets/Player/Player.tsx
+++ apps/rtk-query/src/widgets/Player/Player.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router'
 
 import { useFetchTrackByIdQuery } from '@/features/tracks'
 import type { Track } from '@/player'
