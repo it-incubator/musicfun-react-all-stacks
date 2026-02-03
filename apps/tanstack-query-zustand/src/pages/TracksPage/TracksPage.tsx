8a7fa3bac202cbba5e6e73aad4869ae149923e55
Svyatoslav Nemkovich <svyat5555@gmail.com>
2026-02-03 20:04:58 +0300

    refactor(tracks-page): update player integration and track conversion

--- apps/tanstack-query-zustand/src/pages/TracksPage/TracksPage.tsx
+++ apps/tanstack-query-zustand/src/pages/TracksPage/TracksPage.tsx
@@ -16,6 +16,12 @@ import { useTranslation } from 'react-i18next'
 import { PageWrapper, SearchTextField, SortSelect } from '../common'
 import { useTracksInfinityQuery } from './model/useTracksInfinityQuery.ts'
 import s from './TracksPage.module.css'
+import {
+  convertApiTrackToPlayerTrack,
+  useCurrentTrack,
+  usePlaybackProgress,
+  usePlayerControls,
+} from '@/player'
 
 const PAGE_SIZE = 10
 
@@ -43,7 +49,9 @@ export const TracksPage = () => {
       sortBy,
       sortDirection,
     })
-  const { play, currentTrack, currentTime } = usePlayerStore()
+  const { currentTime } = usePlaybackProgress()
+  const { play } = usePlayerControls()
+  const { track: currentTrack } = useCurrentTrack()
 
   const tracks = React.useMemo(() => {
     return VU.isNotEmptyArray(data?.pages) ? data.pages.map((page) => page.data).flat() : []
@@ -80,13 +88,7 @@ export const TracksPage = () => {
         : void 0
 
       if (track) {
-        play({
-          artist: 'artist',
-          coverSrc: track.attributes.images.main?.[0]?.url,
-          id: track.id,
-          src: track.attributes.attachments[0].url,
-          title: track.attributes.title,
-        })
+        play(convertApiTrackToPlayerTrack(track))
       }
     },
     [tracks, play]
